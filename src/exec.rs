17c0ec8d38b9d226d8f2de5b01258c4713f0b0b1
Link <yingxuw814@gmail.com>
2025-04-14 13:06:13 +0800

    更新 exec.rs

--- src/exec.rs
+++ src/exec.rs
@@ -3,7 +3,7 @@ use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 
-// ========== FrameFuture next_frame().await) ==========
+// Returns Pending as long as its inner bool is false.
 #[derive(Default)]
 pub struct FrameFuture {
     done: bool,
@@ -12,8 +12,11 @@ pub struct FrameFuture {
 impl Future for FrameFuture {
     type Output = ();
 
-    fn poll(mut self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
+    fn poll(mut self: Pin<&mut Self>, _context: &mut Context) -> Poll<Self::Output> {
         if self.done {
+            // We were told to step, meaning this future gets destroyed and we run
+            // the main future until we call next_frame again and end up in this poll
+            // function again.
             Poll::Ready(())
         } else {
             self.done = true;
@@ -22,85 +25,52 @@ impl Future for FrameFuture {
     }
 }
 
-// ========== FileLoadingFuture ==========
-type FileResult<T> = Result<T, std::io::Error>;
+type FileResult<T> = Result<T, crate::file::FileError>;
 
 pub struct FileLoadingFuture {
-    contents: Arc<Mutex<Option<FileResult<Vec<u8>>>>,
-}
-
-impl FileLoadingFuture {
-    pub fn new(path: String) -> Self {
-        let contents = Arc::new(Mutex::new(None));
-        let contents_clone = contents.clone();
-
-        std::thread::spawn(move || {
-            let data = std::fs::read(path);
-            *contents_clone.lock().unwrap() = Some(data);
-        });
-
-        Self { contents }
-    }
+    pub contents: Arc<Mutex<Option<FileResult<Vec<u8>>>>>,
 }
 
 impl Future for FileLoadingFuture {
     type Output = FileResult<Vec<u8>>;
 
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
-        if let Some(result) = self.contents.lock().unwrap().take() {
-            Poll::Ready(result)
+    fn poll(self: Pin<&mut Self>, _context: &mut Context) -> Poll<Self::Output> {
+        let mut contents = self.contents.lock().unwrap();
+        if let Some(contents) = contents.take() {
+            Poll::Ready(contents)
         } else {
             Poll::Pending
         }
     }
 }
 
-// ========== Waker ==========
 fn waker() -> Waker {
-    unsafe fn clone(_data: *const ()) -> RawWaker {
-        RawWaker::new(std::ptr::null(), &VTABLE)
+    unsafe fn clone(data: *const ()) -> RawWaker {
+        RawWaker::new(data, &VTABLE)
     }
     unsafe fn wake(_data: *const ()) {
+        panic!(
+            "macroquad does not support waking futures, please use coroutines, \
+            otherwise your pending future will block until the next frame"
+        )
     }
-    unsafe fn wake_by_ref(_data: *const ()) {
+    unsafe fn wake_by_ref(data: *const ()) {
+        wake(data)
     }
     unsafe fn drop(_data: *const ()) {
+        // Nothing to do
     }
     const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
     let raw_waker = RawWaker::new(std::ptr::null(), &VTABLE);
     unsafe { Waker::from_raw(raw_waker) }
 }
 
-pub fn resume<T>(future: &mut Pin<Box<dyn Future<Output = T>>>) -> Option<T> {
+/// returns Some(T) if future is done, None if it would block
+pub(crate) fn resume<T>(future: &mut Pin<Box<dyn Future<Output = T>>>) -> Option<T> {
     let waker = waker();
-    let mut cx = Context::from_waker(&waker);
-    match future.as_mut().poll(&mut cx) {
+    let mut futures_context = std::task::Context::from_waker(&waker);
+    match future.as_mut().poll(&mut futures_context) {
         Poll::Ready(v) => Some(v),
         Poll::Pending => None,
-    }
-}
-
-#[macroquad::main("Future Test")]
-async fn main() {
-    let mut frame_future = Box::pin(FrameFuture::default());
-    
-    let mut file_future = Box::pin(FileLoadingFuture::new("Cargo.toml".to_string()));
-
-    loop {
-        if let Some(()) = resume(&mut frame_future) {
-            println!("FrameFuture completed!");
-            frame_future = Box::pin(FrameFuture::default());
-        }
-
-        if let Some(result) = resume(&mut file_future) {
-            match result {
-                Ok(data) => println!("Loaded file ({} bytes)", data.len()),
-                Err(e) => println!("Error loading file: {:?}", e),
-            }
-            break;
-        }
-
-        clear_background(WHITE);
-        next_frame().await;
     }
 }
